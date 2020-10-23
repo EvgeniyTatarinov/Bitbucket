f4465a8cd1fc8b3db2a986a2496b489401021be2
evgen <jackson.ru@mail.ru>
2020-10-23 23:12:35 +0500

    23.10.2020 Created Registration and Authorization app

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-import json
-
 from tornado.ioloop import IOLoop
-from tornado.web import Application, RequestHandler, HTTPError
+from tornado.web import Application, RequestHandler
 from tornado.escape import json_decode
 from tornado.options import parse_command_line
 from tornado.httpserver import HTTPServer
 from tornado_sqlalchemy import SQLAlchemy, SessionMixin
 
 from settings import COOKIE_SECRET, DATABASE_URL
-from shema import Url, User
+from shema import User
 from auth import Authorization
 
 
-class UrlApp(SessionMixin, RequestHandler):
-    def set_default_headers(self):
-        self.set_header("Content-Type", 'application/json; charset="utf-8"')
-
-    def get(self, current_user=None):
-        urls_to_user = self.session.query(Url).filter_by(user_id=1)
-        print(urls_to_user.all())
-        self.write(json.dumps(
-            {'status': 200, 'result': f'message {urls_to_user.all()}'}
-        ))
-
-
-class UserApp(SessionMixin, RequestHandler):
+class RegistrationApp(SessionMixin, RequestHandler):
 
     def _check_login_exists(self, login: str) -> bool:
         result = self.session.query(User).filter_by(username=login)
@@ -50,7 +36,7 @@ class UserApp(SessionMixin, RequestHandler):
                     data['login'], hash_password
                 )
                 self.set_secure_cookie("user", data['login'])
-                self.set_status(200)
+                self.set_status(201)
                 self.finish(
                     {'status': self.get_status(), 'id': f'{request_user_id}'}
                 )
@@ -62,15 +48,60 @@ class UserApp(SessionMixin, RequestHandler):
             self.finish({'status': self.get_status(), 'error': 'request failed'})
 
 
+class AuthorizationApp(SessionMixin, RequestHandler):
+    auth = Authorization()
+
+    def get_current_user(self):
+        return self.get_secure_cookie('user')
+
+    def _query_one_user_id(self, *args_filter) -> (int, None):
+        user = self.session.query(User).filter(*args_filter)
+        if user.count() != 0:
+            return user.one().id
+        return None
+
+    def get(self):
+        current_user = self.get_current_user()
+        if not current_user:
+            self.set_status(401)
+            self.finish(
+                {'status': self.get_status(), 'id': 'Not Authorization'}
+            )
+        else:
+            username = bytes.decode(current_user)
+            user_id = self._query_one_user_id(User.username == username)
+            self.set_status(200)
+            self.finish({'status': self.get_status(), 'message': user_id})
+
+    def post(self):
+        basic_data = self.request.headers.get('Authorization')
+        users_data = self.auth.base_auth(basic_data)
+        hash_password = self.auth.hash_function(users_data['login'], users_data['password'])
+        user_id = self._query_one_user_id(
+            User.username == users_data['login'], User.password == hash_password
+        )
+        if user_id:
+            self.set_secure_cookie("user", users_data['login'])
+            self.set_status(200)
+            self.finish(
+                {'status': self.get_status(), 'id': user_id}
+            )
+        else:
+            self.set_status(400)
+            self.finish({'status': self.get_status(), 'error': 'Invalid login or password'})
+
+
 class App(Application):
     def __init__(self):
         handlers = [
             (r"/", UrlApp),
-            (r"/user", UserApp),
+            (r"/registration", RegistrationApp),
+            (r"/login", AuthorizationApp)
         ]
         settings = {
             "cookie_secret": COOKIE_SECRET,
             "db": SQLAlchemy(DATABASE_URL),
+            "login_url": '/login'
         }
         Application.__init__(self, handlers, **settings)
 
